c3a9c2da50e686b7c8c0afde7f534735b408103d
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2024-05-13 15:14:12 +0200

    15:15

--- src/Form/UploadDeviType.php
+++ src/Form/UploadDeviType.php
@@ -7,14 +7,29 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class UploadDeviType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('deviFile', VichFileType::class)
-        ;
+            ->add('deviFile', VichFileType::class,[
+                'required' => true,
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control-file'
+                ],
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Merci de téléverser un fichier PDF valide.',
+                    ])
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/UploadPhotoType.php
+++ src/Form/UploadPhotoType.php
@@ -4,16 +4,34 @@ namespace App\Form;
 
 use App\Entity\Photo;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UploadPhotoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('imageFile', VichFileType::class);
+        ->add('imageFile', VichFileType::class, [
+            'required' => true,
+            'allow_delete' => true,
+            'label' => false,
+            'attr' => [
+                'class' => 'form-control-file',
+            ],
+            'constraints' => [
+                new File([
+                    'mimeTypes' => [
+                        'image/jpeg',
+                        'image/pjpeg',
+                        'image/webp',
+                    ],
+                    'mimeTypesMessage' => 'Merci de téléverser un fichier JPG, JPEG ou WEBP valide.',
+                ])
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
