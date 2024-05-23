3a184f498b373d02fbbe1fa607c3dc8a6fe0ac48
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2024-05-23 15:04:23 +0200

    formulaire ajout client OK

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
-use App\Repository\CategorieRepository;
+use App\Form\AjoutTacheType;
+use App\Form\AjoutClientType;
 use App\Repository\ClientRepository;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\InterventionRepository;
-use App\Repository\CategorieTacheRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,15 +18,28 @@ class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
 
-    public function index(ClientRepository $ClientRepository, InterventionRepository $InterventionRepository, CategorieRepository $categorieTacheRepository): Response
+    public function index(EntityManagerInterface $entityManager ,Request $request, AjoutTacheType $ClientForm, ClientRepository $ClientRepository, InterventionRepository $InterventionRepository, CategorieRepository $categorieTacheRepository): Response
     {   
         // Si l'utilisateur est connecté
         if ($this->getUser()) { 
         
-            // Récupération des clients et des interventions
+                // Récupération des clients et des interventions
                 $clients = $ClientRepository->findAll();
                 $interventionsEncours = $InterventionRepository->findby(['statut' => 'En cours']);
                 $categorieTaches = $categorieTacheRepository->findAll();
+
+                // Création du formulaire d'ajout de client
+                $formAjoutClient = $this->createForm(AjoutClientType::class);
+
+                // Traitement du formulaire d'ajout de client
+                $formAjoutClient->handleRequest($request);
+
+                if ($formAjoutClient->isSubmitted() && $formAjoutClient->isValid()) {
+                    $client = $formAjoutClient->getData();
+                    $entityManager->persist($client);
+                    $entityManager->flush();
+                    return $this->redirectToRoute('main');
+                }
 
                 // Affichage de la page d'accueil
                 return $this->render('main/index.html.twig', [
@@ -35,7 +51,8 @@ class MainController extends AbstractController
                     'Clients' => $clients,
                     'categorieTaches' => $categorieTaches,
                     'interventions' => $interventionsEncours,
-                    'visibility' => 'd-block'
+                    'visibility' => 'd-block',
+                    'formAjoutClient' => $formAjoutClient->createView()
                 ]);
         
         // Sinon redirection vers la page d'accueil
@@ -44,7 +61,6 @@ class MainController extends AbstractController
         }
     }   
 
-    
     #[Route('/interList/{list}', name: 'interList')]
     public function afficheSelectInter(string $list, ClientRepository $ClientRepository, InterventionRepository $InterventionRepository): Response
     {    
